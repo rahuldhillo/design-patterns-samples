866f6cb487dd12ab422155b4528731430db35736
rahuldhillo <anemailformac@gmail.com>
2023-03-26 11:42:17 +0530

    singleton pattern complete

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { DemoService } from 'src/app/services/demo.service';
 
 @Component({
   selector: 'app-demo',
-  templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.css']
+  template: `
+    <button (click)="setDataInService()">Set Data</button>
+    <button (click)="getDataFromService()">Get Data</button>
+  `,
 })
 export class DemoComponent {
+  constructor(private demoService: DemoService) {}
 
+  setDataInService(): void {
+    this.demoService.setData({ message: 'Hello World!' });
+  }
+
+  getDataFromService(): void {
+    console.log(this.demoService.getData());
+  }
 }
--- src/app/services/demo.service.ts
+++ src/app/services/demo.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DemoService {
+  private data: any;
 
-  constructor() { }
+  constructor() {}
+
+  setData(data: any): void {
+    this.data = data;
+  }
+
+  getData(): any {
+    return this.data;
+  }
 }
